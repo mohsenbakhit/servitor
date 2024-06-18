e70fc1a1efc4518982989132a799b6d1d8ca1b7c
Mohsen Bakhit <bakhitmohsen@proton.me>
2024-06-17 22:55:48 -0700

    Create socket, Bind socket to port

--- journal.md
+++ journal.md
@@ -4,3 +4,7 @@ In this project I will be a lot about networking and how socket programming in C
 ### June 15, 2024
 The start of the project. So far I have finished close to 3 chapters from "Beej's guide to Networking". I have learnt about different sockets, what they do, which protocols they use, what TCP and UDP are different on and IP addresses.
 
+### June 17, 2024
+Finished reading Beej's guide. It was great for understanding what the functions and structs in the C socket libraries are and do
+but I still have to figure things out. So far, I have managed to create and bind a socket to a port given through standard input.
+
--- src/servitor.c
+++ src/servitor.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#define PORT 8080
 
-int main(int argc, char** argv) {
-	return 0;
+int main(int argc, char const** argv) {
+	// AF_LOCAL is for processes on the same host
+	// AF_INET is for IPv4, and AF_INET6 is for IPv6
+	int server_fd, new_socket;
+	struct sockaddr_in address;
+	socklen_t addrlen = sizeof(address);
+	int opt = 1;
+	int port = atoi(argv[1]);
+	if (server_fd = socket(AF_INET, SOCK_STREAM, 0) < 0) {
+		perror("ERROR: Socket failed");
+		exit(EXIT_FAILURE);
+	}
+
+	if (setsockopt(server_fd, SOL_SOCKET,
+			   SO_REUSEADDR | SO_REUSEPORT, &opt,
+			   sizeof(opt))) {
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
+			   }
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(port);
+
+	if (bind(server_fd, (struct sockaddr*)&address, addrlen) < 0) {
+		perror("Error: Bind failed");
+		exit(EXIT_FAILURE);
+	}
 }
